3d688fbc06ab94c6c88203241ead60202dc9deee
Rohit Kumar Mahato <mahator99@gmail.com>
2023-05-16 20:39:48 +0530

    Minified the size of pictures and updated some links

--- assets/app.js
+++ assets/app.js
@@ -1,27 +1,3 @@
-var swiper = new Swiper(".project", {
-  grabCursor: true,
-  loop: true,
-  effect: "creative",
-  autoplay: {
-    delay: 2000,
-    disableOnInteraction: false,
-  },
-  creativeEffect: {
-    prev: {
-      shadow: false,
-      translate: ["-120%", 0, -500],
-    },
-    next: {
-      shadow: false,
-      translate: ["120%", 0, -500],
-    },
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-  },
-});
-
 (function () {
   "use strict";
   window.addEventListener(
